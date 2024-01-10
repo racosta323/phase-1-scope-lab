eb5df926f32a3ca45ba6c66312ee50296e3251e5
Rene <racosta323@gmail.com>
2024-01-10 15:29:59 -0600

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'some initial value'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'bob'
+}
+
+
+
+// customerName: Declare a variable in global scope called customerName using the var keyword and assign it the value 'bob'.
+// upperCaseCustomerName(): Write a function that accesses that global customerName variable, and uppercases it.
+
+
